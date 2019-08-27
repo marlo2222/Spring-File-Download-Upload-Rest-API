c5f599d8733a34ebe66ed29c9cce2e790f626c5b
marlo2222 <marlo_henrique@hotmail.com.br>
2019-08-27 12:42:39 -0300

    documentação swagger

--- usuario/src/main/java/com/api/usuario/controller/UsuarioController.java
+++ usuario/src/main/java/com/api/usuario/controller/UsuarioController.java
@@ -2,6 +2,8 @@ package com.api.usuario.controller;
 
 import com.api.usuario.model.Usuario;
 import com.api.usuario.repository.UsuarioRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@Api(value = "API de usuarios")
+@CrossOrigin("*/")
 public class UsuarioController {
 
     @Autowired
@@ -18,31 +22,36 @@ public class UsuarioController {
 
     @GetMapping(value = "/")
     public String home(){
-        return "Hello World";
+        return "Acesse seu endereço local /swagger-ui.html#!/";
     }
 
     @GetMapping(value = "/usuario/listar/")
+    @ApiOperation(value = "Retorna uma lista de usuarios")
     public List<Usuario> listarUsuarios(){
         return usuarioRepository.findAll();
     }
 
     @GetMapping(value = "/usuario/listar/{id}")
+    @ApiOperation(value = "Retorna um usuario pelo id")
     public Usuario listarUsuarioId(@PathVariable("id") long id){
         return usuarioRepository.getOne(id);
     }
 
     @PostMapping(value = "/usuario/salvar")
+    @ApiOperation(value = "Salva um usuario")
     public Usuario salvarUsuario(@RequestBody Usuario usuario){
         return usuarioRepository.save(usuario);
     }
 
     @DeleteMapping(value = "/usuario/deletar/{id}")
+    @ApiOperation(value = "Deleta um usuario pelo id")
     public ResponseEntity<String> deletarUsuario(@PathVariable("id") long id){
         usuarioRepository.deleteById(id);
         return new ResponseEntity<String>("Usuario deletado com sucesso", HttpStatus.OK);
     }
 
     @PutMapping(value = "/usuario/atualizar")
+    @ApiOperation(value = "Retorna uma lista de atualiza um usuario")
     public Usuario atualizarUsuario(@RequestBody Usuario usuario){
         return usuarioRepository.save(usuario);
     }
